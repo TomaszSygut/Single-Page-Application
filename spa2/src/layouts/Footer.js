c3c24f12f20c0e93cea6ca6448ccdff6d556b04e
TomaszSygut <thomassygut@gmail.com>
2020-03-29 17:28:39 +0200

    footer

--- spa2/src/layouts/Footer.js
+++ spa2/src/layouts/Footer.js
@@ -1,10 +1,31 @@
 import React from 'react'
-
+import { Route } from 'react-router-dom'
+import '../styles/Footer.css'
 const Footer = () => {
     return (
         <div>
-            Footer
-        </div>
+            <h2>Stopka</h2>
+            <Route path="/:page/" exact render={(props) => {
+                return (
+                    <>
+                        <p> Jesteś na <span>{props.match.params.page}</span></p>
+                        <p> Jesteś na <span>{props.match.url}</span></p>
+                        <p> Jesteś na <span>{props.match.path}</span></p>
+                    </>
+                )
+            }} />
+
+
+            <Route path="/:page/:idProduktu" exact render={(props) => {
+                return (
+                    <>
+                        <p> Jesteś na <span>{props.match.params.idProduktu}</span></p>
+                        <p> Jesteś na <span>{props.match.url}</span></p>
+                        <p> Jesteś na <span>{props.match.path}</span></p>
+                    </>
+                )
+            }} />
+        </div >
     )
 }
 
