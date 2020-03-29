f9f709a902b7921c00c4f7686c8763c22d8cdad1
TomaszSygut <thomassygut@gmail.com>
2020-03-29 14:25:47 +0200

    header

--- spa2/src/layouts/Header.js
+++ spa2/src/layouts/Header.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import img1 from '../images/street.jpg';
+import img2 from '../images/laguna.jpg';
+import img3 from '../images/sunset.jpg';
+import '../styles/Header.css';
 
 const Header = () => {
+
     return (
-        <div>
-            Header
-            
-        </div>
+        <>
+            <Switch>
+                <Route path="/" exact render={() => (
+                    <img src={img1} alt="miasto" />
+                )} />
+                <Route path="/products" render={() => (
+                    <img src={img3} alt="miasto" />
+                )} />
+                <Route path="/contact" render={() => (
+                    <img src={img2} alt="miasto" />
+                )} />
+                <Route path="/admin" render={() => (
+                    <img src={img1} alt="miasto" />
+                )} />
+                <Route render={() => (
+                    <img src={img3} alt="miasto" />
+                )} />
+            </Switch>
+
+
+        </>
     )
 }
 
